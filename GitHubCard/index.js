b76e645fc88e209d7944d69af5a8f136b9f1f059
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-03 18:12:45 -0400

    mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,28 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cardContainer = document.querySelector(".cards");
 
+const followersArray = ["https://api.github.com/users/JChartier001", "https://api.github.com/users/cjogles", "https://api.github.com/users/AMC-ai"];
+
+
+axios.get("https://api.github.com/users/indiMjc").then(response => {
+    const newUser = CardMaker;
+    cardContainer.appendChild(newUser(response.data));
+})
+.catch(error => {
+  console.log("The data was not returned", error);
+});
+
+followersArray.map(ele => {
+  axios.get(ele).then(response => {
+    const newUser = CardMaker;
+    cardContainer.appendChild(newUser(response.data));
+  })
+})
+.catch(error => {
+  console.log("The data was not returned", error);
+});
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -17,14 +38,14 @@
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          at the bottom of the page. Get at least 5 different Github users and add them as
           Individual strings to the friendsArray below.
           
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -33,7 +54,7 @@ const followersArray = [];
   <img src={image url of user} />
   <div class="card-info">
     <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
+    <p class="user">{users user name}</p>
     <p>Location: {users location}</p>
     <p>Profile:  
       <a href={address to users github page}>{address to users github page}</a>
@@ -43,10 +64,54 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
 
-/* List of LS Instructors Github username's: 
+
+function CardMaker (userData) {
+  //create elements
+  let card = document.createElement("div");
+  let userImg = document.createElement("img");
+  let cardInfo = document.createElement("div");
+  let user = document.createElement("h3");
+  let userName = document.createElement("p");
+  let location = document.createElement("p");
+  let profile = document.createElement("p");
+  let profileLink = document.createElement("a");
+  let follower = document.createElement("p");
+  let followingCount = document.createElement("p");
+  let bio = document.createElement("p");
+  //classes
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  user.classList.add("name")
+  userName.classList.add("user");
+  //populate
+  userImg.src = userData.avatar_url;
+  user.textContent = userData.name;
+  userName.textContent = `User Name: ${userData.login}`;
+  location.textContent = `Location: ${userData.location}`;
+  profile.textContent = `Profile: `;
+  profileLink.textContent = userData.html_url;
+  profileLink.setAttribute("href", userData.hrml_url);
+  follower.textContent = `Followers: ${userData.followers}`;
+  followingCount.textContent = `Following: ${userData.following}`;
+  bio.textContent = `Bio: ${userData.bio}`;
+  //structure
+  card.appendChild(userImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(user);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink);
+  cardInfo.appendChild(follower);
+  cardInfo.appendChild(followingCount);
+  cardInfo.appendChild(bio);
+
+  return card;
+}
+
+/* List of LS Instructors Github user's: 
   tetondan
   dustinmyers
   justsml
